0005ff1ca931e0e96cdd87c12f009e3639102084
Juauzin <rjoao3185@gmail.com>
2023-03-28 15:53:28 -0300

    Operadores

--- tipos-variaveis/src/App.java
+++ tipos-variaveis/src/App.java
@@ -1,5 +1,11 @@
 public class TipoVariaveis {
+   
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+final  double IDADE = 12;
+    IDADE = 12;
+    
+
     }
 }
---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,39 @@
+import javax.swing.event.SwingPropertyChangeSupport;
+
+public class Operadores {
+    public static void main(String[] args) {
+
+        String nomeUm = "JOAO";
+        String nomeDois = "JOAO";
+
+       System.out.println( nomeUm == nomeDois      );
+
+    }
+
+
+
+        int numero1 = 1;
+        int numero2 = 2;
+        
+        boolean simNao = numero1 == numero2;
+        if(numero1 < numero2){
+            System.out.println("a nossa condição é verdadeira");
+
+        }
+
+        System.out.println("numeroUm é igual a numeroDois? " + simNao);
+ 
+        simNao = numero1 == numero2;
+
+        System.out.println("numeroUm é diferente a numeroDois? " + simNao);
+ 
+        simNao = numero1 == numero2;
+
+        System.out.println("numeroUm é maior que numeroDois? " + simNao);
+ 
+
+    }
+    
+    }
+    
+
